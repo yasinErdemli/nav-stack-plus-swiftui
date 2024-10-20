32233042158f8920f687ce90cc3a41105cf3685b
Yasin Erdemli <yasinerdemli35@icloud.com>
2024-10-20 20:35:05 +0300

    folder impovements and better preview for CustomNavigationHeaderContainerView

---
+++ Sources/CustomNavStack/Views/CustomNavigationHeader.swift
@@ -0,0 +1,147 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by Yasin Erdemli on 19/8/24.
+//
+
+import SwiftUI
+
+struct CustomNavigationHeader: View {
+    let labels: [CustomToolbarItem]
+    let background: EquatableViewContainer
+    let backgroundOpacity: CGFloat
+    let backButtonDisabled: Bool
+    let offset: CGFloat
+    @State private var headerHeight: CGFloat = .zero
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.navigationController) var navigationController
+    @Environment(\.layoutDirection) var layoutDirection
+    @State private var previousControllers: [UIViewController] = []
+    var body: some View {
+        HStack {
+            Spacer()
+                .overlay(alignment: layoutDirection == .leftToRight ? .leading : .trailing) {
+                    HStack {
+                        if !isFirstController && !backButtonDisabled {
+                            CustomHeaderBackButton(
+                                headerHeight: $headerHeight,
+                                previousControllers: previousControllers)
+                        }
+                        ForEach(layoutDirection == .leftToRight ? leadingLabels : trailingLabels) { label in
+                            label.content
+                                .background {
+                                    GeometryReader { geo in
+                                        Color.clear
+                                            .task {
+                                                headerHeight = max(headerHeight, geo.size.height)
+                                            }
+                                    }
+                                }
+                        }
+                    }
+                }
+            if let principalLabel {
+                principalLabel.content
+                    .background {
+                        GeometryReader { geo in
+                            Color.clear
+                                .task {
+                                    headerHeight = max(headerHeight, geo.size.height)
+                                }
+                        }
+                    }
+            }
+            Spacer()
+                .overlay(alignment: layoutDirection == .leftToRight ? .trailing : .leading) {
+                    HStack {
+                        ForEach(layoutDirection == .leftToRight ? trailingLabels : leadingLabels) { label in
+                            label.content
+                                .background {
+                                    GeometryReader { geo in
+                                        Color.clear
+                                            .task {
+                                                headerHeight = max(headerHeight, geo.size.height)
+                                            }
+                                    }
+                                }
+                        }
+                    }
+                }
+        }
+        .frame(height: headerHeight)
+        .font(.title3)
+        .labelStyle(.iconOnly)
+        .buttonStyle(.plain)
+        .padding(.horizontal, 16)
+        .padding(.bottom, 8)
+        .background {
+            background
+                .padding(.horizontal, -16)
+                .padding(.top, -80)
+                .opacity(backgroundOpacity)
+        }
+        .opacity((labels.isEmpty && (isFirstController || backButtonDisabled)) ? 0 : 1)
+        .frame(height: (labels.isEmpty && (isFirstController || backButtonDisabled)) ? 0 : nil)
+        .onAppear {
+            self.previousControllers = getPreviousControllers()
+        }
+        .offset(y: -offset)
+    }
+
+}
+
+extension CustomNavigationHeader {
+    private func getPreviousControllers() -> [UIViewController] {
+        guard let viewControllers = navigationController?.viewControllers else {
+            return []
+        }
+        return viewControllers.dropLast()
+    }
+
+    private var isFirstController: Bool {
+        return previousControllers.isEmpty
+    }
+}
+
+extension CustomNavigationHeader {
+    private var trailingLabels: [CustomToolbarItem] {
+        getValues(for: labels, selection: .trailing, max: 2)
+    }
+    private var principalLabel: CustomToolbarItem? {
+        getValues(for: labels, selection: .principal, max: 1).last
+    }
+    private var leadingLabels: [CustomToolbarItem] {
+        getValues(for: labels, selection: .leading, max: 2)
+    }
+    private func getValues(
+        for items: [CustomToolbarItem],
+        selection: CustomToolbarItemPlacement,
+        max value: Int) -> [CustomToolbarItem] {
+            let usableItems = items.filter { $0.placement == selection }
+            let toBeDeletedCount = abs(min(0, value - usableItems.count))
+            return usableItems.dropFirst(toBeDeletedCount).map { $0 }.reversed()
+        }
+}
+
+#Preview {
+    CustomNavigationStack {
+        Rectangle()
+            .fill(.blue)
+            .ignoresSafeArea()
+            .customToolbar {
+                CustomToolbarItem(placement: .leading) {
+                    Button("Profile", systemImage: "person.circle.fill", action: { })
+                        .font(.title)
+                }
+                CustomToolbarItem(placement: .principal) {
+                    Text("Custom Nav Stack")
+                }
+                CustomToolbarItem(placement: .trailing) {
+                    Button("More Options", systemImage: "ellipsis.circle.fill", action: {})
+                        .foregroundStyle(.purple)
+                }
+            }
+            .customToolbarBackground(opacity: 0.4)
+    }
+}
---
+++ Sources/CustomNavStack/Views/CustomNavigationLink.swift
@@ -0,0 +1,129 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Yasin Erdemli on 24/8/24.
+//
+
+import SwiftUI
+import ScrollPlus
+
+struct CustomNavigationLink<Label: View, Destination: View>: View {
+    let destination: Destination?
+    let label: Label
+    let value: AnyHashable?
+    init(@ViewBuilder destination: () -> Destination, @ViewBuilder label: () -> Label) {
+        self.destination = destination()
+        self.label = label()
+        self.value = nil
+    }
+    var body: some View {
+        NavigationLink {
+            CustomNavigationHeaderContainerView {
+                destination
+            }
+            .toolbar(.hidden, for: .navigationBar)
+        } label: {
+            label
+        }
+    }
+}
+
+extension CustomNavigationLink where Destination == Never {
+    init<P>(value: P?, @ViewBuilder label: () -> Label) where P: Hashable {
+        self.value = value
+        self.label = label()
+        self.destination = nil
+    }
+    init<P>(_ titleKey: LocalizedStringKey, value: P?) where Label == Text, P: Hashable {
+        self.label = .init(titleKey)
+        self.value = value
+        self.destination = nil
+    }
+    init<S, P>(_ title: S, value: P?) where Label == Text, S: StringProtocol, P: Hashable {
+        self.label = .init(title)
+        self.value = value
+        self.destination = nil
+    }
+    init<P>(value: P?, @ViewBuilder label: () -> Label) where P: Decodable, P: Encodable, P: Hashable {
+        self.value = value
+        self.label = label()
+        self.destination = nil
+    }
+    var body: some View {
+        NavigationLink(value: value) {
+            label
+        }
+    }
+}
+
+extension CustomNavigationLink where Label == Text {
+    init(_ titleKey: LocalizedStringKey, @ViewBuilder destination: () -> Destination) {
+        self.label = .init(titleKey)
+        self.destination = destination()
+        self.value = nil
+    }
+
+    var body: some View {
+        NavigationLink {
+            CustomNavigationHeaderContainerView {
+                destination
+            }
+            .toolbar(.hidden, for: .navigationBar)
+        } label: {
+            label
+        }
+    }
+}
+
+#Preview {
+    return ExampleView()
+    struct ExampleView: View {
+        @State private var opacity: CGFloat = 0
+        var body: some View {
+            CustomNavigationStack {
+                List(1..<10) { number in
+                    NavigationLink("Select \(number) ", value: number)
+                        .listRowBackground(Rectangle().fill(.red))
+                }
+                .customToolbar {
+                    CustomToolbarItem(placement: .leading) {
+                        Text("Hello")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .foregroundStyle(.blue)
+                    }
+
+                    CustomToolbarItem(placement: .trailing) {
+                        Text("Tarot")
+                            .font(.largeTitle)
+                    }
+                    CustomToolbarItem(placement: .trailing) {
+                        Text("Tarot")
+                            .font(.largeTitle)
+                    }
+                }
+                .customToolbarBackground {
+                    Rectangle()
+                        .fill(.ultraThinMaterial)
+                        .ignoresSafeArea()
+                }
+                .customNavigationDestination(for: Int.self) { _ in
+                    GeometryScrollView {
+                        ForEach(0..<20) { _ in
+                            Rectangle()
+                                .fill(.red)
+                                .frame(height: 200)
+                        }
+                    }
+                    .navigationTitle("world")
+                    .customToolbarBackground {
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .ignoresSafeArea()
+                    }
+                }
+                .navigationTitle("hello")
+            }
+        }
+    }
+}
