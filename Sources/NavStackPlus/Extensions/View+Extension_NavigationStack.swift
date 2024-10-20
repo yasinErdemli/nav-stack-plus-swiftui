eaba3df0a07a58d902b583341464885154eab61c
Yasin Erdemli <yasinerdemli35@icloud.com>
2024-10-21 00:44:33 +0300

    add documentation to navigationDestinationPlus

--- Sources/NavStackPlus/Extensions/View+Extension_NavigationStack.swift
+++ Sources/NavStackPlus/Extensions/View+Extension_NavigationStack.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Yasin Erdemli on 21/8/24.
 //
@@ -10,17 +10,62 @@ import SwiftUI
 import ScrollPlus
 
 extension View {
+    /// Associates a destination view with a presented data type for use within
+    /// a navigation stack.
+    ///
+    /// Add this view modifier to a view inside a ``NavigationStackPlus`` to
+    /// describe the view that the stack displays when presenting
+    /// a particular kind of data. Use a ``NavigationLinkPlus`` to present
+    /// the data. For example, you can present a `ColorDetail` view for
+    /// each presentation of a ``Color`` instance:
+    ///
+    ///    NavigationStackPlus {
+    ///        Rectangle()
+    ///            .fill(.blue)
+    ///            .ignoresSafeArea()
+    ///            .toolbarPlus {
+    ///                ToolbarItemPlus(placement: .leading) {
+    ///                    Button("Profile", systemImage: "person.circle.fill", action: { })
+    ///                        .font(.title)
+    ///                }
+    ///                ToolbarItemPlus(placement: .principal) {
+    ///                    Text("Custom Nav Stack")
+    ///                }
+    ///                ToolbarItemPlus(placement: .trailing) {
+    ///                    Button("More Options", systemImage: "ellipsis.circle.fill", action: {})
+    ///                        .foregroundStyle(.purple)
+    ///                }
+    ///            }
+    ///            .toolbarBackgroundPlus(opacity: 0.4)
+    ///    }
+    ///
+    /// You can add more than one navigation destination modifier to the stack
+    /// if it needs to present more than one kind of data.
+    ///
+    ///
+    /// Do not put a navigation destination modifier inside a "lazy" container,
+    /// like ``List`` or ``LazyVStack``. These containers create child views
+    /// only when needed to render on screen. Add the navigation destination
+    /// modifier outside these containers so that the navigation stack can
+    /// always see the destination.
+    ///
+    /// - Parameters:
+    ///   - data: The type of data that this destination matches.
+    ///   - destination: A view builder that defines a view to display
+    ///     when the stack's navigation state contains a value of
+    ///     type `data`. The closure takes one argument, which is the value
+    ///     of the data to present.
     public func navigationDestinationPlus<D, C>(
         for data: D.Type,
         @ViewBuilder destination: @escaping (D) -> C) -> some View where D: Hashable, C: View {
-        self
-            .navigationDestination(for: data) { item in
-                CustomNavigationHeaderContainerView {
-                    destination(item)
+            self
+                .navigationDestination(for: data) { item in
+                    CustomNavigationHeaderContainerView {
+                        destination(item)
+                    }
+                    .toolbar(.hidden, for: .navigationBar)
                 }
-                .toolbar(.hidden, for: .navigationBar)
-            }
-    }
+        }
 }
 
 extension View {
@@ -30,7 +75,7 @@ extension View {
             .preference(key: ToolbarPreferenceKey.self, value: content())
     }
 
-   @ViewBuilder
+    @ViewBuilder
     public func toolbarBackgroundPlus<Content: View>(@ViewBuilder _ background: () -> Content) -> some View {
         let value = EquatableViewContainer(content: background)
         self
